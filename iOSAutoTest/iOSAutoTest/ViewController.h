6b18abaacec5403e0e33fe7b3bc993531df2e8d6
DaXiang <1295434840@qq.com>
2018-05-08 14:43:43 +0800

    1
    1

---
+++ iOSAutoTest/iOSAutoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSAutoTest
+//
+//  Created by Eric on 2018/5/8.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSAutoTest/iOSAutoTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSAutoTest
+//
+//  Created by Eric on 2018/5/8.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
